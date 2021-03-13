e04a06154aa8f0c3bfa79996e07331b437f2a83c
djdtimit <djdtimit@aol.comm>
2021-03-13 23:08:24 +0100

    added new schema structure, added first view in raw schema and added input_file_name()

--- Projects/Covid/ETL.sql
+++ Projects/Covid/ETL.sql
@@ -64,11 +64,7 @@
 
 -- COMMAND ----------
 
-CREATE DATABASE IF NOT EXISTS COVID
-
--- COMMAND ----------
-
-USE COVID
+CREATE DATABASE IF NOT EXISTS COVID_INGESTION
 
 -- COMMAND ----------
 
@@ -82,7 +78,7 @@ USE COVID
 
 -- COMMAND ----------
 
-CREATE TABLE IF NOT EXISTS TBL_country_vaccinations_INGESTION (
+CREATE TABLE IF NOT EXISTS covid_ingestion.TBL_country_vaccinations (
     country STRING,
     iso_code STRING,
     date STRING,
@@ -104,12 +100,16 @@ LOCATION "/mnt/kaggle/Covid/Ingestion/covid_19_world_vaccination_progress/countr
 
 -- COMMAND ----------
 
+Source",input_file_name()
+
+-- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC **covid_DE**
 
 -- COMMAND ----------
 
-CREATE TABLE IF NOT EXISTS TBL_COVID_DE_INGESTION (
+CREATE TABLE IF NOT EXISTS covid_ingestion.TBL_COVID_DE (
 state STRING,
 country STRING,
 age_group STRING,
@@ -129,7 +129,7 @@ LOCATION "/mnt/kaggle/Covid/Ingestion/covid19-tracking-germany/covid_de.csv"
 
 -- COMMAND ----------
 
-CREATE TABLE IF NOT EXISTS TBL_DEMOGRAPHICS_DE_INGESTION (
+CREATE TABLE IF NOT EXISTS covid_ingestion.TBL_DEMOGRAPHICS_DE (
 STATE STRING,
 GENDER STRING,
 AGE_GROUP STRING,
@@ -145,7 +145,7 @@ LOCATION '/mnt/kaggle/Covid/Ingestion/covid19-tracking-germany/demographics_de.c
 
 -- COMMAND ----------
 
-CREATE TABLE IF NOT EXISTS TBL_Worldometer_coronavirus_daily_data_INGESTION (
+CREATE TABLE IF NOT EXISTS covid_ingestion.TBL_Worldometer_coronavirus_daily_data (
 DATE STRING,
 COUNTRY STRING,
 CUMULATIVE_TOTAL_CASES STRING,
@@ -164,7 +164,7 @@ LOCATION '/mnt/kaggle/Covid/Ingestion/covid19-global-dataset/worldometer_coronav
 
 -- COMMAND ----------
 
-CREATE TABLE IF NOT EXISTS TBL_worldometer_coronavirus_summary_data_INGESTION (
+CREATE TABLE IF NOT EXISTS covid_ingestion.TBL_worldometer_coronavirus_summary_data (
 country STRING,
 CONTINENT STRING,
 TOTAL_CONFIRMED STRING,
@@ -187,7 +187,33 @@ LOCATION "/mnt/kaggle/Covid/Ingestion/covid19-global-dataset/worldometer_coronav
 
 -- COMMAND ----------
 
-CREATE TABLE IF NOT EXISTS TBL_country_vaccinations_RAW (
+CREATE DATABASE IF NOT EXISTS covid_raw
+
+-- COMMAND ----------
+
+CREATE OR REPLACE VIEW covid_raw.VW_country_vaccinations
+AS SELECT 
+country,
+iso_code,
+date,
+total_vaccinations,
+people_vaccinated,
+people_fully_vaccinated,
+daily_vaccinations_raw,
+daily_vaccinations,
+total_vaccinations_per_hundred,
+people_vaccinated_per_hundred,
+people_fully_vaccinated_per_hundred,
+daily_vaccinations_per_million,
+vaccines,
+source_name,
+source_website,
+INPUT_FILE_NAME() as source
+FROM covid_ingestion.TBL_country_vaccinations
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS covid_raw.TBL_country_vaccinations (
     country STRING,
     iso_code STRING,
     date STRING,
@@ -204,31 +230,38 @@ CREATE TABLE IF NOT EXISTS TBL_country_vaccinations_RAW (
     source_name STRING,
     source_website STRING,
     INSERT_TS STRING,
-    UPDATE_TS STRING
+    UPDATE_TS STRING,
+    SOURCE STRING
 ) USING DELTA
-LOCATION "/mnt/kaggle/Covid/Raw/covid_19_world_vaccination_progress/"
+LOCATION "/mnt/kaggle/Covid/Raw/covid_19_world_vaccination_progress/country_vaccinations/"
 
 -- COMMAND ----------
 
-MERGE INTO TBL_country_vaccinations_RAW AS T
+MERGE INTO covid_raw.TBL_country_vaccinations AS T
 USING 
-TBL_country_vaccinations_INGESTION S
+covid_raw.VW_country_vaccinations S
 on 
 T.country = S.country AND
 T.iso_code = S.iso_code AND
-T.date = S.date AND
-T.total_vaccinations = S.total_vaccinations AND
-T.people_vaccinated = S.people_vaccinated AND
-T.people_fully_vaccinated = S.people_fully_vaccinated AND
-T.daily_vaccinations_raw = S.daily_vaccinations_raw AND
-T.daily_vaccinations = S.daily_vaccinations AND
-T.total_vaccinations_per_hundred = S.total_vaccinations_per_hundred AND
-T.people_vaccinated_per_hundred = S.people_vaccinated_per_hundred AND
-T.people_fully_vaccinated_per_hundred = S.people_fully_vaccinated_per_hundred AND
-T.daily_vaccinations_per_million = S.daily_vaccinations_per_million AND
-T.vaccines = S.vaccines AND
-T.source_name = S.source_name AND
-T.source_website = S.source_website
+T.date = S.date 
+
+WHEN MATCHED THEN 
+UPDATE SET 
+T.total_vaccinations = S.total_vaccinations,
+T.people_vaccinated = S.people_vaccinated,
+T.people_fully_vaccinated = S.people_fully_vaccinated,
+T.daily_vaccinations_raw = S.daily_vaccinations_raw,
+T.daily_vaccinations = S.daily_vaccinations,
+T.total_vaccinations_per_hundred = S.total_vaccinations_per_hundred,
+T.people_vaccinated_per_hundred = S.people_vaccinated_per_hundred,
+T.people_fully_vaccinated_per_hundred = S.people_fully_vaccinated_per_hundred,
+T.daily_vaccinations_per_million = S.daily_vaccinations_per_million,
+T.vaccines = S.vaccines,
+T.source_name = S.source_name,
+T.source_website = S.source_website,
+T.UPDATE_TS = current_timestamp,
+T.source = S.source
+
 WHEN NOT MATCHED
 THEN INSERT ( country,
 iso_code,
@@ -244,7 +277,7 @@ people_fully_vaccinated_per_hundred,
 daily_vaccinations_per_million,
 vaccines,
 source_name,
-source_website, insert_ts, update_ts ) 
+source_website, insert_ts, update_ts, source ) 
 VALUES (
 S.country,
 S.iso_code,
@@ -261,7 +294,104 @@ S.daily_vaccinations_per_million,
 S.vaccines,
 S.source_name,
 S.source_website, 
-current_timestamp, current_Timestamp)
+current_timestamp, current_Timestamp, source)
+
+-- COMMAND ----------
+
+select * from covid_raw.TBL_country_vaccinations
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC **Covid_DE**
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS covid_raw.TBL_COVID_DE (
+state STRING,
+country STRING,
+age_group STRING,
+gender STRING,
+date STRING,
+cases STRING,
+death STRING,
+recoverd STRING,
+INSERT_TS STRING,
+UPDATE_TS STRING) 
+USING DELTA
+LOCATION "/mnt/kaggle/Covid/Raw/covid19-tracking-germany/Covid_DE/"
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC **Demographics_DE**
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS covid_raw.TBL_DEMOGRAPHICS_DE (
+STATE STRING,
+GENDER STRING,
+AGE_GROUP STRING,
+POPULATION STRING,
+INSERT_TS STRING,
+UPDATE_TS STRING)
+USING DELTA
+LOCATION '/mnt/kaggle/Covid/Raw/covid19-tracking-germany/Demographics_DE/'
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC **Worldometer_coronavirus_daily_data**
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS covid_raw.TBL_Worldometer_coronavirus_daily_data (
+DATE STRING,
+COUNTRY STRING,
+CUMULATIVE_TOTAL_CASES STRING,
+DAILY_NEW_CASES STRING,
+ACTIVE_CASES STRING,
+CUMULATIVE_TOTAL_DEATHS STRING,
+DAILY_NEW_DEATHS STRING,
+INSERT_TS STRING,
+UPDATE_TS STRING)
+USING DELTA
+LOCATION '/mnt/kaggle/Covid/Raw/covid19-global-dataset/worldometer_coronavirus_daily_data/'
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS covid_raw.TBL_worldometer_coronavirus_summary_data (
+country STRING,
+CONTINENT STRING,
+TOTAL_CONFIRMED STRING,
+TOTAL_DEATHS STRING,
+TOTAL_RECOVERED STRING,
+ACTIVE_CASES STRING,
+SERIOUS_OR_CRITICAL STRING,
+TOTAL_CASES_PER_LM_POPULATION STRING,
+TOTAL_TESTS STRING,
+TOTAL_TESTS_PER_LM_POPULATION STRING,
+POPULATION STRING,
+INSERT_TS STRING,
+UPDATE_TS STRING)
+USING DELTA
+LOCATION "/mnt/kaggle/Covid/Raw/covid19-global-dataset/worldometer_coronavirus_summary_data/"
+
+-- COMMAND ----------
+
+
 
 -- COMMAND ----------
 
